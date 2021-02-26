f524273550fdafe23bd746627567083303152a8d
Tracy Velasquez <velasqueztracy25@gmail.com>
2021-02-25 20:32:02 -0600

    removed package

--- BattleshipBust.java
+++ BattleshipBust.java
@@ -1,5 +1,3 @@
-package battleship2;
-
 import java.util.ArrayList;
 
 public class BattleshipBust {
--- GameHelper.java
+++ GameHelper.java
@@ -1,5 +1,3 @@
-package battleship2;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
